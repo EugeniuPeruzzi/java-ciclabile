e9764b2af1e0c7604db89dc1b5c978725ef1b89b
EugeniuPeruzzi <jaka1997@live.it>
2023-11-17 15:19:12 +0100

    exercise 3 done

--- src/org/lessons/java/CicloItero.java
+++ src/org/lessons/java/CicloItero.java
@@ -5,6 +5,45 @@ public class CicloItero {
 	private int indice;
 	
 	public CicloItero(int[] numeri) {
-		
+		setNumeri(numeri);
+		setIndice(indice);
 	}
+
+	public int[] getNumeri() {
+		return numeri;
+	}
+
+	public void setNumeri(int[] numeri) {
+		this.numeri = numeri;
+	}
+
+	public int getIndice() {
+		return indice;
+	}
+
+	public void setIndice(int indice) {
+		this.indice = indice;
+	}
+	
+    public int getElementoSuccessivo() {
+        int[] elemento = getNumeri();
+        setIndice(indice + 1);
+        return elemento[getIndice() - 1];
+    }
+
+    public boolean hasAncoraElementi() {
+        return getIndice() < getNumeri().length;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Numeri: ");
+        for (int numero : numeri) {
+            sb.append(numero + " ");
+        }
+        sb.append("\nIndice: " + indice);
+        return sb.toString();
+    }
+    
 }
--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -3,8 +3,14 @@ package org.lessons.java;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	    
+		int[] numeri = {0, 1, 2, 3, 4, 5};
 
+		CicloItero c1 = new CicloItero(numeri);
+		
+		while (c1.hasAncoraElementi()) {			
+			System.out.println(c1.getElementoSuccessivo());
+		}
 	}
 
 }
